9ea7ac52c9d03fed78bfa274579ccb6c17cb3632
Fawaz Ahmed <fawazahmed0@hotmail.com>
2021-04-30 18:03:24 +0530

    Update hardcodesubv2.js

--- hardcodesubv2.js
+++ hardcodesubv2.js
@@ -679,7 +679,7 @@ async function getSubLink (title, page) {
       await page.evaluate(el => el.click(), subtitlesTab[0])
       await page.waitForNavigation()
       await page.waitForSelector('[id="video-title"]')
-      await page.waitForFunction('document.querySelectorAll(\'[id="video-title"]\').length > 5')
+      await page.waitForFunction('document.querySelectorAll(\'[id="video-title"]\').length > 1')
       console.log("title text is ",title)
       console.log("fetching sublink")
       subLink = await page.evaluate(titletext => Array.from(document.querySelectorAll('[id="video-title"]')).map(e => [e.textContent.trim(), e.href]).filter(e => e[0]!=="" && titletext.toLowerCase().includes(e[0].slice(0,-3).toLowerCase()) && /.*?translations$/.test(e[1]))[0][1], title)
